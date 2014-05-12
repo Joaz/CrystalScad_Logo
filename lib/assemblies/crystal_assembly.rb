d2536d8d39356de00873fa6ecdf5c537693dd118
Joachim Glauche <jglauche@tardis.(none)>
2014-05-12 21:58:34 +0200

    and the actual work from today

--- lib/assemblies/crystal_assembly.rb
+++ lib/assemblies/crystal_assembly.rb
@@ -1,9 +1,17 @@
 class CrystalAssembly < CrystalScad::Assembly
 
 	def part(show)
-		res = Crystal.new.show.color(r:0	,b:255,g:255, a:100)
+		crystal = Crystal.new
+		gear = PrintedGear.new(module:1.5,teeth:42,height:8,bore:8)
+				
+  	res = crystal.show
+	  res += gear.translate(z:crystal.height).color("lightgrey")
 	
 		res
 	end
+
+  def description
+    "CrystalSCAD Logo"
+  end
 
 end
--- lib/printed/crystal.rb
+++ lib/printed/crystal.rb
@@ -1,16 +1,26 @@
 class Crystal < CrystalScad::Printed
+
 	def initialize()
 		@d1 = 0.1
-		@d2= 10
-		@cone_height = 10
-		@middle_height = 2	
+		@d2 = 100
+		@d3 = 65
+		@cone_height = 90
+		@middle_height = 8	
+		@top_height = 8 
 		@fn = 9
+		
+		@height = @cone_height + @middle_height + @top_height
 	end
 
 	def part(show)
-		res = cylinder(d1:@d1,d2:@d2,h:@cone_height ,fn:@fn)
-		res += cylinder(d:@d2, h:@middle_height, fn:@fn).translate(z:@cone_height)
-		res += cylinder(d1:@d2,d2:@d1,h:10,fn:@fn).translate(z:@cone_height+@middle_height)
+		res = cylinder(d1:@d1,d2:@d2,h:@cone_height,fn:@fn)
+		res += cylinder(d:@d2, h:@middle_height,fn:@fn).translate(z:@cone_height)
+		res += cylinder(d1:@d2,d2:@d3,h:@top_height,fn:@fn).translate(z:@cone_height+@middle_height)		
+
+		if show
+		  res = res.color(r:0,b:255,g:255,a:210)
+		end
+
 		res
 	end
 
